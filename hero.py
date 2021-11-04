a3d97b38e43ee4dc73383b153c5f21961e16930b
Brooklyn Dippo <brooklyndippo@gmail.com>
2021-11-03 19:19:26 -0500

    added more methods to hero, complex fighting

--- hero.py
+++ hero.py
@@ -1,37 +1,81 @@
 import random
+from ability import Ability
+from armor import Armor
 
 class Hero:
-  def __init__(self, name, starting_health=100):
-    '''Instance properties:
-      name: String
-      starting_health: Integer
-      current_health: Integer
-    '''
-    self.name = name
-    self.starting_health = starting_health
-    self.current_health = starting_health
+    def __init__(self, name, starting_health=100):
+        self.name = name
+        self.starting_health = starting_health
+        self.current_health = starting_health
+        self.abilities = list()
+        self.armors = list()
 
-  def fight(self, opponent):
-      total_power = self.current_health + opponent.current_health
+    # add OFFENSIVE abilities
+    def add_ability(self, ability):
+        self.abilities.append(ability)
 
-      #calculate the odds of each winning
-      hero_odds = self.current_health/total_power*100
-      opp_odds = opponent.current_health/total_power*100
-      print(hero_odds)
-      print(opp_odds)
+    def attack(self):
+        total_damage = 0
+        for ability in self.abilities:
+            total_damage += ability.attack()
+        return total_damage
 
-      winning_num = random.randint(0,100)
-      #if the random 1-100 number is less than percent chance of hero1 winning, make winner
-      if winning_num < hero_odds:
-          print(f'{self.name} defeats {opponent.name}.')
-      else:
-          print(f'{opponent.name} defeats {self.name}.')
+    # add DEFENSIVE abilities
+    def add_armor(self, armor):
+        self.armors.append(armor)
+
+    def defend(self):
+        total_block = 0
+        for armor in self.armors:
+            total_block += armor.block()
+        return total_block   
+
+    def take_damage(self, damage):
+        defense = self.defend()
+        if defense < damage:
+            self.current_health -= (damage - defense)
+        return self.current_health
+
+    #match up opponents for fight
+    def fight(self, opponent):
+        total_power = self.current_health + opponent.current_health
+
+        if self.abilities == [] and opponent.abilities == []:
+            print('DRAW')
+            return
+
+        #run the fighting while both heros are still alive
+        while self.is_alive() == True and opponent.is_alive() == True:
+            print('fighting')
+            self.take_damage(opponent.attack())
+            #print(f'slf: {self.current_health}')
+            opponent.take_damage(self.attack())
+            #print(f'opp: {opponent.current_health}')
+
+        #print the results once one or both players are dead
+        if self.current_health <= 0 and opponent.current_health <= 0:
+            print('DRAW')
+
+        elif self.current_health > opponent.current_health:
+            print(f'{self.name} defeats {opponent.name}.')
+
+        else:
+            print(f'{opponent.name} defeats {self.name}.')
+
+    def is_alive(self):
+        if self.current_health <= 0:
+            return False
+        else:
+            return True
 
 
 
 #run test code only in this file
 if __name__ == "__main__":
-    hero1 = Hero("Wonder Woman", 450)
-    hero2 = Hero("Dumbledore", 200)
-    hero1.fight(hero2)
-
+    slf = Hero("Grace Hopper")
+    opp = Hero("Dumbledore")
+    ability = Ability('snort', 30)
+    opp.add_ability(ability)
+    slf.add_ability(ability)
+    print(slf.abilities)
+    slf.fight(opp)
