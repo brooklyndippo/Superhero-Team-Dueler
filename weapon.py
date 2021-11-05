59ca5d2b16cd84f9cbf0e07fb8ecc0c8d1933eb2
Brooklyn Dippo <brooklyndippo@gmail.com>
2021-11-05 16:13:53 -0500

    created teams and passed pytest

--- weapon.py
+++ weapon.py
@@ -4,7 +4,7 @@ import random
 class Weapon(Ability):
 
     def attack(self):
-        half_damage = self.max_damage/2
+        half_damage = self.max_damage//2
         attack_amount = random.randint(half_damage, self.max_damage)
         return attack_amount
 
